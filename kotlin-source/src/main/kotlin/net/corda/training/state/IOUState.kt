351aae5cdd2275a0871df56a15896572d8e2581f
Nandansv <nandansvamc1993@hotmail.com>
2018-10-31 22:13:54 +0530

    Testcases#10,11 Passed

--- kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
@@ -6,6 +6,7 @@ import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
+import net.corda.finance.DOLLARS
 import net.corda.finance.POUNDS
 import java.util.*
 
@@ -18,9 +19,15 @@ import java.util.*
 data class IOUState(val amount: Amount<Currency>,
                     val lender: Party,
                     val borrower: Party,
-                    val paid: Amount<Currency> = 0.POUNDS,
+                    val paid: Amount<Currency> = 0.DOLLARS,
                     override val linearId: UniqueIdentifier = UniqueIdentifier()): ContractState, LinearState {
 
     override val participants: List<Party> get() = listOf(lender, borrower)
+
+    // Below function will execute states creation per every transaction undergone..
+
+    fun pay(amttoPay : Amount<Currency> ) = copy(paid = paid.plus(amttoPay))
+
+    fun withNewLender(newLender : Party) = copy(lender = newLender)
 
 }
