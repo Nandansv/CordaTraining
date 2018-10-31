7adcc403c7cac03601dbc005d6e0e659706b5ebd
Nandansv <nandansvamc1993@hotmail.com>
2018-10-31 18:37:56 +0530

    Testcase#7 Passed

--- kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
@@ -2,6 +2,8 @@ package net.corda.training.state
 
 import net.corda.core.contracts.Amount
 import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
 import net.corda.finance.POUNDS
@@ -12,16 +14,13 @@ import java.util.*
  * instructions on how to complete the [IOUState] class.
  *
  * Remove the "val data: String = "data" property before starting the [IOUState] tasks.
- */
+**/
 data class IOUState(val amount: Amount<Currency>,
                     val lender: Party,
-                    val borrower : Party,
-                    val paid: Amount<Currency> = 0.POUNDS ): ContractState {
-    override val participants: List<Party> get() = listOf(borrower, lender)
+                    val borrower: Party,
+                    val paid: Amount<Currency> = 0.POUNDS,
+                    override val linearId: UniqueIdentifier = UniqueIdentifier()): ContractState, LinearState {
+
+    override val participants: List<Party> get() = listOf(lender, borrower)
+
 }
-
-//class IOUState {
-//   // this.lender =
-//
-//}
-
